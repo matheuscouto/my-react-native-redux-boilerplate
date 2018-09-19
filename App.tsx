55df14485a791ad6d775d564bc85cd03eab75885
Matheus Couto <couto.matheus@outlook.com>
2018-09-19 16:14:13 -0300

    feat: Redux and React Native Debugger working properly

--- App.tsx
+++ App.tsx
@@ -8,6 +8,9 @@
 
 import * as React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Provider } from 'react-redux';
+
+import store from './src/store';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,11 +23,13 @@ type Props = {};
 export default class App extends React.Component<Props> {
   render() {
     return (
+			<Provider store={store}>
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
+			</Provider>
     );
   }
 }
